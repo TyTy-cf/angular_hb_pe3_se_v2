eb5f7ff7c69a642d97944f68c9f6f5233eda6757
Kevin Tourret <tarpion@toto.fr>
2022-03-10 15:47:52 +0100

    output

--- src/app/random-user-card/random-user-card.component.ts
+++ src/app/random-user-card/random-user-card.component.ts
@@ -1,5 +1,5 @@
-import {Component, Input} from '@angular/core';
-import { faHeart, faCrosshairs } from '@fortawesome/free-solid-svg-icons';
+import {Component, EventEmitter, Input, OnChanges, Output, SimpleChanges} from '@angular/core';
+import {faHeart, faCrosshairs} from '@fortawesome/free-solid-svg-icons';
 import {ApiRandomUser} from "../../models/api-randomuser/api-random-user";
 
 @Component({
@@ -7,13 +7,30 @@ import {ApiRandomUser} from "../../models/api-randomuser/api-random-user";
   templateUrl: './random-user-card.component.html',
   styleUrls: ['./random-user-card.component.scss']
 })
-export class RandomUserCardComponent {
+export class RandomUserCardComponent implements OnChanges {
 
   faCrosshairs = faCrosshairs;
   iconFaHeart = faHeart;
 
+  // Lors d'un OutPut, on passe TOUJOURS un objet de type EventEmitter
+  // le type à l'intérieur est celui qui sera transmis de la fille vers la mère
+  @Output()
+  eventEmitterNewProfile: EventEmitter<boolean> = new EventEmitter<boolean>();
+
   @Input()
   randomUserDaughter: ApiRandomUser|undefined;
   smallDisplay: boolean = false;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.smallDisplay = false;
+  }
+
+  notifyMotherComponent(): void {
+    // dans la fonction "emit" je passe en argument un attribut/objet du même type
+    // que déclarer dans l'EventEmitter
+    // lorsque la fonction emit est appelée, c'est à ce moment-là que l'information remonte
+    // de la fille vers la mère
+    this.eventEmitterNewProfile.emit(true);
+  }
 
 }
--- src/app/random-user/random-user.component.ts
+++ src/app/random-user/random-user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ApiRandomUserService} from "../../services/api-random-user.service";
 import {ApiRandomUser} from "../../models/api-randomuser/api-random-user";
+import {$e} from "@angular/compiler/src/chars";
 
 @Component({
   selector: 'app-random-user',
@@ -19,7 +20,10 @@ export class RandomUserComponent implements OnInit {
     // this.apiRandomUserService.getRandomUserAny().subscribe((jsonResponse) => {
     //   console.log(jsonResponse);
     // });
+    this.newProfile(true);
+  }
 
+  newProfile(event: boolean): void {
     this.apiRandomUserService.getRandomUser().subscribe((jsonResponse) => {
       // getRandomUser() renvoie un Observable<ApiRandomUserResults>
       // jsonResponse est de type ApiRandomUserResults, donc je peux accéder aux attributs de cette classe/interface
@@ -27,8 +31,6 @@ export class RandomUserComponent implements OnInit {
       // ici je choisis de prendre le 1er élément de mon tableau car mon API renvoie un tableau
       // avec un seul élément dedans - ça m'évite de traiter un tableau par la suite
       this.randomUser = jsonResponse.results[0];
-      console.log(this.randomUser);
     });
   }
-
 }
